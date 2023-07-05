56ffd5f124e371ae9f8d7b49ab905540253e0d5a
Davydov <hi@fax1ty.dev>
2023-07-06 01:18:39 +0300

    👌 IMPROVE: Simplified colliders

--- src/objects/crown/index.tsx
+++ src/objects/crown/index.tsx
@@ -1,15 +1,22 @@
 import { Gltf, useGLTF } from "@react-three/drei";
-import { RigidBody, RigidBodyProps } from "@react-three/rapier";
+import {
+  CylinderCollider,
+  RigidBody,
+  RigidBodyProps,
+} from "@react-three/rapier";
 
 import { useRandomForce } from "../use-random-force";
 import model from "./model.glb";
 
+const HEIGHT = 0.67;
+
 export const Crown = (props: RigidBodyProps) => {
-  const api = useRandomForce(-40, 40);
+  const api = useRandomForce(-20, 20);
 
   return (
-    <RigidBody {...props} ref={api} colliders="hull">
-      <Gltf src={model} />
+    <RigidBody {...props} ref={api} colliders={false}>
+      <Gltf src={model} position-y={-HEIGHT} />
+      <CylinderCollider args={[HEIGHT, 1]} />
     </RigidBody>
   );
 };
--- src/objects/microphone/index.tsx
+++ src/objects/microphone/index.tsx
@@ -1,15 +1,20 @@
 import { Gltf, useGLTF } from "@react-three/drei";
-import { RigidBody, RigidBodyProps } from "@react-three/rapier";
+import {
+  CapsuleCollider,
+  RigidBody,
+  RigidBodyProps,
+} from "@react-three/rapier";
 
 import { useRandomForce } from "../use-random-force";
 import model from "./model.glb";
 
 export const Microphone = (props: RigidBodyProps) => {
-  const api = useRandomForce(-20, 20);
+  const api = useRandomForce(-10, 10);
 
   return (
-    <RigidBody {...props} ref={api} colliders="hull" rotation-x={-Math.PI / 2}>
+    <RigidBody {...props} ref={api} colliders={false} rotation-x={-Math.PI / 2}>
       <Gltf src={model} />
+      <CapsuleCollider args={[0.75, 0.42]} />
     </RigidBody>
   );
 };
